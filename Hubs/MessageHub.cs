022d43c23bf867f42cdc05f47fc0123cd1b49a38
Hasan BOZKUŞ <hhasanbozkus0147@gmail.com>
2023-11-27 15:41:34 +0300

    Add project files.

---
+++ Hubs/MessageHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalRChatExample.Hubs
+{
+    public class MessageHub : Hub
+    {
+        public async Task SendingMessageAsync(string message, string userName)
+        {
+            await Clients.All.SendAsync("receivedMessage", message, userName);
+        }
+    }
+}

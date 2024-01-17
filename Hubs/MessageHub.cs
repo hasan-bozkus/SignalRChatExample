fe6d2293984966ebcab8773ce01777324b93bdf9
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-01-17 20:30:15 +0300

    Asp .Net Core SignalR 5 - SignalR'da Bağlantı Olayları

--- Hubs/MessageHub.cs
+++ Hubs/MessageHub.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace SignalRChatExample.Hubs
 {
     public class MessageHub : Hub
     {
+        static List<string> clients = new List<string>();
+
         public async Task SendingMessageAsync(string message, string userName)
         {
             await Clients.All.SendAsync("receivedMessage", message, userName);
@@ -13,11 +16,15 @@ namespace SignalRChatExample.Hubs
 
         public async override Task OnConnectedAsync()
         {
+            clients.Add(Context.ConnectionId);
+            await Clients.All.SendAsync("clients", clients);
             await Clients.All.SendAsync("JoinedUser", $"{Context.ConnectionId}");
         }
 
         async public override Task OnDisconnectedAsync(Exception exception)
         {
+            clients.Remove(Context.ConnectionId);
+            await Clients.All.SendAsync("clients", clients);
             await Clients.All.SendAsync("LeavedUser", $"{Context.ConnectionId}");
         }
     }
